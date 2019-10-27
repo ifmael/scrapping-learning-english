0ab40f5a622e926af8bb1c36ed755e1fed3d2085
Ismael Rodriguez <=>
2019-10-27 21:58:19 +0000

    Download files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const puppeteer = require('puppeteer');
+const download  = require('download-file');
+
+
 
 (async () => {
   try {
-    const browser = await puppeteer.launch({devtools: true}); // {devtools: true} if you want debug inside evaluate function
+    const browser = await puppeteer.launch(); // {devtools: true} if you want debug inside evaluate function
     const page = await browser.newPage();
-
+    const listMp3Url = [];
     await page.goto('http://www.bbc.co.uk/learningenglish/english/features/6-minute-english');
 
     const listUrlPages = await page.evaluate(() => {
@@ -19,17 +22,35 @@ const puppeteer = require('puppeteer');
     });
 
 
-    listUrlPages.forEach( async urlPage => {
-      debugger;
-      await page.goto(urlPage);
-      const x = await page.evaluate(() => {
-        debugger;
-       /*  const aTag = document.querySelector('.bbcle-download-extension-mp3');
-        debugger;
-        return listUrl; */
-      });
-    }); 
+    const tinyArray = listUrlPages.slice(0,4);
 
+    for(let i=0; i < tinyArray.length -1;i++){
+      await page.goto(tinyArray[i]);
+      const urlMp3Download = await page.evaluate(() => {
+        return document.querySelector('.bbcle-download-extension-mp3').href;
+      });
+
+      const urlMp3Split = urlMp3Download.split('/');
+      const filename = urlMp3Split[urlMp3Split.length - 1];
+
+      const options = {
+        directory: "./download",
+        filename
+      }
+
+      download(urlMp3Download, options, function(err){
+        if (err) throw err
+        listMp3Url.push(urlMp3Download);
+      });
+    }
+
+
+
+
+
+    debugger;
+
+    page.close()
     await browser.close();
   } catch (error){
     console.error(error);
